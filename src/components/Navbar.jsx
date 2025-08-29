eabc696fcadcd1fc9ac47af411fa79df6db712de
thanhphong39 <phongntse181679@fpt.edu.vn>
2025-08-29 14:14:39 +0700

    Add Page

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import {assets} from '../assets/assets'
+import { NavLink } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-between py-5 font-medium">
+      <img src={assets.logo} className="w-36" alt="" />
+      <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
+        <NavLink to="/" className="flex flex-col items-center gap-1">
+          <p>HOME</p>
+          <hr className="w-6 border-2 border-black rounded-full hidden" />
+        </NavLink>
+        <NavLink to="/collection" className="flex flex-col items-center gap-1 ">
+          <p>COLLECTION</p>
+          <hr className="w-6 border-2 border-black rounded-full hidden" />
+        </NavLink>
+        <NavLink to="/about" className="flex flex-col items-center gap-1">
+          <p>ABOUT</p>
+          <hr className="w-6 border-2 border-black rounded-full hidden" />
+        </NavLink>
+        <NavLink to="/contact" className="flex flex-col items-center gap-1">
+          <p>CONTANT</p>
+          <hr className="w-6 border-2 border-black rounded-full hidden" />
+        </NavLink>
+      </ul>
+
+      <div className="flex items-center gap-6">
+        <img src={assets.search_icon} className="w-5 cursor-pointer" alt="" />
+        <div className="group relative">
+            <img src={assets.profile_icon} className="w-5 cursor-pointer" alt="" />
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default Navbar

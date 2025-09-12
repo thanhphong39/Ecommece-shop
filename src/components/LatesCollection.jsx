b59d2ff790fa08871812833b218e508d77fc8dc6
thanhphong39 <phongntse181679@fpt.edu.vn>
2025-09-12 21:25:11 +0700

    Update Laset

--- src/components/LatesCollection.jsx
+++ src/components/LatesCollection.jsx
@@ -1,18 +1,18 @@
-import React, { use, useEffect } from 'react'
-import { ShopContext } from '../context/ShopContext';
-import Title from './Title';
+import React, { useEffect } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Title from "./Title";
+import ProductItem from "./ProductItem";
 
 const LatesCollection = () => {
+  const { products } = React.useContext(ShopContext);
+  const [latestProducts, setLatestProducts] = React.useState([]);
 
-    const {products} = React.useContext(ShopContext);
-    const [latestProducts, setLatestProducts] = React.useState([]);
-
-    useEffect(() => {
-        setLatestProducts(products.slice(0, 10));
-    }, []);
+  useEffect(() => {
+    setLatestProducts(products.slice(0, 10));
+  }, [products]);
 
   return (
-    <div className='my-10'>
+    <div className="my-10">
       <div className="text-center py-8 text-3xl">
         <Title text1={"LATEST"} text2={"COLLECTION"} />
         <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
@@ -22,10 +22,20 @@ const LatesCollection = () => {
           limits.
         </p>
       </div>
-      
 
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {latestProducts.map((item, index) => (
+          <ProductItem
+            key={index}
+            id={item._id}
+            image={item.image}
+            name={item.name}
+            price={item.price}
+          />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
-export default LatesCollection
+export default LatesCollection;

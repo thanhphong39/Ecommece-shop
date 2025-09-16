3338a934e76658f2c8fa27eb3c35d2fbe93f7b28
thanhphong39 <phongntse181679@fpt.edu.vn>
2025-09-16 21:30:12 +0700

    Collections

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -5,14 +5,21 @@ import { Link } from 'react-router';
 const ProductItem = ({id,image,name,price}) => {
     const {currency} = React.useContext(ShopContext);
   return (
-    <Link to={`/product/${id}`} className='text-gray-700 cursor-pointer'>
-        <div className='overflow-hidden'>
-            <img className='hover:scale-110 transition ease-in-out' src={image[0]} alt='' />
-        </div>
-        <p className='pt-3 pb-1 text-sm'>{name}</p>
-        <p className='text-sm font-medium'>{currency}{price}</p>
+    <Link to={`/product/${id}`} className="text-gray-700 cursor-pointer">
+      <div className="overflow-hidden">
+        <img
+          className="hover:scale-110 transition ease-in-out rounded-md "
+          src={image[0]}
+          alt=""
+        />
+      </div>
+      <p className="pt-3 pb-1 text-sm">{name}</p>
+      <p className="text-sm font-medium">
+        {currency}
+        {price}
+      </p>
     </Link>
-  )
+  );
 }
 
 export default ProductItem
--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,41 +1,56 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { ShopContext } from "../context/ShopContext";
+import { assets } from "../assets/assets";
+import Title from "../components/Title";
+import ProductItem from "../components/ProductItem";
+
 
 const Collection = () => {
   const { products } = useContext(ShopContext);
-  const [showFillter, setShowFilter] = useState(false);
+  const [showFilter, setShowFilter] = useState(false);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+
+  useEffect(() => {
+    setFilteredProducts(products);
+  }, []);
 
   return (
-    <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
+    <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t border-gray-200">
       <div className="min-w-60">
-        <p className="my-2 text-xl flex items-center cursor-pointer gap-2">
+        <p
+          onClick={() => setShowFilter(!showFilter)}
+          className="my-2 text-xl flex items-center cursor-pointer gap-2"
+        >
           FILTERS
+          <img
+            className={`h-3 sm:hidden ${showFilter ? "rotate-90" : ""}`}
+            src={assets.dropdown_icon}
+            alt=""
+          />
         </p>
         <div
           className={`border border-gray-300 pl-5 py-3 mt-6 ${
-            showFillter ? "" : "hidden"
+            showFilter ? "" : "hidden"
           } sm:block`}
         >
           <p className="mb-3 text-sm font-medium">CATEGORIES</p>
           <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
             <p className="flex gap-2">
-              <input className="w-3" type="checkbox" value={"Men"} />{" "}
-              Men
+              <input className="w-3" type="checkbox" value={"Men"} /> Men
             </p>
             <p className="flex gap-2">
-              <input className="w-3" type="checkbox" value={"Women"} />{" "}
-              Women
+              <input className="w-3" type="checkbox" value={"Women"} /> Women
             </p>
             <p className="flex gap-2">
-              <input className="w-3" type="checkbox" value={"Kids"} />{" "}
-              Kids
+              <input className="w-3" type="checkbox" value={"Kids"} /> Kids
             </p>
           </div>
         </div>
 
         <div
-          className={`border border-gray-300 pl-5 py-3 mt-6 ${
-            showFillter ? "" : "hidden"
+          className={`border border-gray-300 pl-5 py-3 my-5 ${
+            showFilter ? "" : "hidden"
           } sm:block`}
         >
           <p className="mb-3 text-sm font-medium">TYPE</p>
@@ -53,6 +68,29 @@ const Collection = () => {
               Winterwear
             </p>
           </div>
+        </div>
+      </div>
+
+      <div className="flex-1">
+        <div className="flex justify-between text-base sm:text-2xl mb-4">
+          <Title text1={"ALL"} text2={"COLLECTION"} />
+          <select className="border-2 border-gray-300 text-sm px-2">
+            <option value="relavent">Sort by Popularity</option>
+            <option value="low-high">Price - Low to High</option>
+            <option value="high-low">Price - High to Low</option>
+          </select>
+        </div>
+
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+          {filteredProducts.map((item, index) => (
+            <ProductItem
+              key={index}
+              id={item._id}
+              image={item.image}
+              name={item.name}
+              price={item.price}
+            />
+          ))}
         </div>
       </div>
     </div>
